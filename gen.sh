2208db0f30a06b6af87a44bceaa7a1124779c385
DatBogie <masterbuildermm2009@gmail.com>
2025-05-28 07:18:33 -0400

    venv3_13

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source .qtcreator/Python_3_13_2venv/bin/activate
+source .qtcreator/Python_3_13_3venv/bin/activate
 pyside6-uic form.ui -o ui_form.py
 pyside6-uic about.ui -o ui_about.py
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source .qtcreator/Python_3_13_2venv/bin/activate
-python3 mainwindow.py
